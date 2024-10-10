bf4a53e19c078d81496f63a27806acaa27955194
Arko-009 <154334525+Arko-009@users.noreply.github.com>
2024-10-10 10:30:13 +0530

    Add files via upload

---
+++ GroupF_project1/recycling-centers.js
@@ -0,0 +1,17 @@
+var recyclingCenters = [
+
+{
+        name: "Delhi Recycling Center",
+        address: "New Delhi, India",
+        coordinates: [28.6139, 77.2090]
+    },
+    {
+        name: "Mumbai Recycling Center",
+        address: "Mumbai, Maharashtra, India",
+        coordinates: [19.0760, 72.8777]
+    }
+
+  // ekhane baki city ja pabi  ------------ 
+
+
+];
